27d5b312cffc07e27572ee8f5586de16d54e1be1
willblood <ledmw19@gmail>
2025-09-12 12:00:51 +0000

    Create chat action and Repository

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,4 +7,28 @@ class ChatsController < ApplicationController
   def show
     @chat = ChatRepository.find(params[:id])
   end
+
+  def create
+    other_user = UserRepository.find(params[:user_id])
+    if other_user
+      result = CreateChat.execute(current_user: current_user, other_user: other_user)
+      if result.success?
+        flash[:success] = "New chat created !"
+      elsif result.message == "Chat already exists between the selected users."
+        chat = find_chat(current_user, other_user)
+        redirect_to chat_path(chat.id)
+      else
+        flash[:errors] = "Failed to create User"
+      end
+    end
+    flash[:errors] = "Selected user doesn't exist"
+  end
+
+  private
+
+  def find_chat(current_user, other_user)
+    name =[current_user.id, other_user.id].sort.join("_")
+    ChatRepository.find_by(name: name)
+  end
+
 end
